18357540811c26634e7cd1f25b2eb3213ffcd88e
mehrabFronto <mehrabd20@gmail.com>
2024-04-15 17:51:03 +0330

    feat: layout and home screen have been added

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-3xl">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link
+        href="/profile"
+        style={{ color: 'blue' }}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
