98c0d007ccc2cb6984e7aa41f70661e0abf3fee8
mehrabFronto <mehrabd20@gmail.com>
2024-04-20 17:16:10 +0330

    feat(home-screen): header of page has been added

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,55 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
+import SearchInput from '../../components/SearchInput';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <View>
+            <Text className="text-3xl text-white">{item.id}</Text>
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            {/* Welcome Text And Logo */}
+            <View className="flex-row justify-between items-start mb-6">
+              {/* Welcome Text */}
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="font-psemibold text-2xl text-white">Mehrab</Text>
+              </View>
+
+              {/* Logo */}
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            {/* Search */}
+            <SearchInput placeholder="Search for a video topic" />
+
+            {/* Latest Videos Title */}
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos
+              </Text>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
